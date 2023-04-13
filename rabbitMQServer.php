0541cba358d8665d9e0f2199cf8c2773cf8dfe6f
slevesque63 <123518095+slevesque63@users.noreply.github.com>
2023-04-13 11:59:15 -0400

    initial commit

---
+++ rabbitMQServer.php
@@ -0,0 +1,33 @@
+
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('getData.php');
+
+function requestProcessor($request) {
+  	echo "received request".PHP_EOL;
+  	//var_dump($request);
+  	if(!isset($request['type'])) {
+    		return "ERROR: unsupported message type";
+  	}
+	switch ($request['type'])
+  	{
+		case "Request":
+			return getData($request['search'], "search");
+			break;
+		case "Lookup":
+			echo $request['showID'];
+			return getData($request['showID'], "lookup");
+			break;
+	}
+	//echo 'here';
+	//echo $request['type'];
+  	//return array("returnCode" => '0', 'message'=>"dmz error");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
